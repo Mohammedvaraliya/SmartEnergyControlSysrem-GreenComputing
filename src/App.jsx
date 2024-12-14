b1927eed0a8da8621d77d2154f157a7ff37347b1
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2024-12-14 11:43:25 +0530

    Updated the router from Browserouter to Hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
   );
 }
 
